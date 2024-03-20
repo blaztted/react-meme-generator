bebb73fe6044813b634db72bebd9c7c63c7d3927
blaztted <bbc.silva@gmail.com>
2024-03-20 18:59:51 +0000

    added array with memesData and click function for the button

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,12 +1,26 @@
 import React from "react";
+import memesData from "../memesData.js";
 
 function Meme() {
+  let url;
+
+  function handleClick(e) {
+    e.preventDefault();
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    url = memesArray[randomNumber].url;
+    console.log(url);
+  }
+
   return (
     <main>
+      <p>{url}</p>
       <form className="form">
         <input type="text" placeholder="Top text" className="form--input" />
         <input type="text" placeholder="Bottom text" className="form--input" />
-        <button className="form--button">Create a new meme image 🖼</button>
+        <button onClick={handleClick} className="form--button">
+          Create a new meme image 🖼
+        </button>
       </form>
     </main>
   );
